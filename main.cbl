a724242994944c1d9ccd7ea638b776a59af304f6
odelbos <od@phibox.com>
2025-04-23 18:42:01 +0200

    move mini map rendering in subprogram

--- main.cbl
+++ main.cbl
@@ -14,28 +14,6 @@
        COPY DD-WORLD-DATA.
        COPY DD-GAME-STATE.
 
-       01 WS-MAP-CELL-W   COMP-1 VALUE ZERO.
-       01 WS-MAP-CELL-H   COMP-1 VALUE ZERO.
-
-       01 WS-MAP-WALL     PIC 9(1) VALUE ZERO.
-
-       01 WS-TMP-X         COMP-1 VALUE ZERO.
-       01 WS-TMP-Y         COMP-1 VALUE ZERO.
-
-       01 WS-X            PIC 9(2) VALUE ZERO.
-       01 WS-Y            PIC 9(2) VALUE ZERO.
-
-       01 WS-P1-X        PIC 9(3) VALUE ZERO.
-       01 WS-P1-Y        PIC 9(3) VALUE ZERO.
-
-       01 WS-P2-X        PIC 9(3) VALUE ZERO.
-       01 WS-P2-Y        PIC 9(3) VALUE ZERO.
-
-       01 WS-RECT-W       PIC 9(3) VALUE ZERO.
-       01 WS-RECT-H       PIC 9(3) VALUE ZERO.
-
-       01 WS-RADIUS       COMP-1 VALUE ZERO.
-
        PROCEDURE DIVISION.
 
       * Initialize game state
@@ -48,102 +26,22 @@
            MOVE 5.5 TO PLAYER-X.
            MOVE 6.5 TO PLAYER-Y.
 
+      * Create window
            CALL "rlInitWindow" USING
                BY VALUE WS-WINDOW-WIDTH
                BY VALUE WS-WINDOW-HEIGHT
                BY REFERENCE WS-WINDOW-TITLE.
 
+      * Main event loop
            PERFORM UNTIL WS-SHOULD-CLOSE = 1
                CALL "rlBeginDrawing"
                CALL "rlClearBackground" USING BY VALUE 0
-               PERFORM RENDER-MAP
-               PERFORM RENDER-MAP-PLAYER
+               CALL "MINIMAP" USING GAME-STATE
                CALL "rlEndDrawing"
                CALL "rlWindowShouldClose" RETURNING WS-SHOULD-CLOSE
            END-PERFORM.
 
            CALL "rlCloseWindow".
-
            STOP RUN.
-
-       RENDER-MAP.
-
-         CALL "rlDrawRectangle" USING
-               BY VALUE MAP-X MAP-Y MAP-WIDTH
-               MAP-HEIGHT, 1
-
-         COMPUTE WS-MAP-CELL-W = MAP-WIDTH / WS-WORLD-WIDTH
-         COMPUTE WS-MAP-CELL-H = MAP-HEIGHT / WS-WORLD-HEIGHT
-
-         PERFORM VARYING WS-Y FROM 1 BY 1
-                   UNTIL WS-Y > WS-WORLD-HEIGHT
-
-               PERFORM VARYING WS-X FROM 1 BY 1
-                     UNTIL WS-X > WS-WORLD-WIDTH
-
-                   MOVE WS-WORLD(WS-Y, WS-X) TO WS-MAP-WALL
-
-                   IF WS-MAP-WALL > 0 THEN
-                      COMPUTE WS-P1-X =
-                         (WS-X - 1) * WS-MAP-CELL-W + MAP-X
-
-                      COMPUTE WS-P1-Y =
-                         (WS-Y - 1) * WS-MAP-CELL-H + MAP-Y
-
-                      COMPUTE WS-RECT-W =
-                         FUNCTION INTEGER(WS-MAP-CELL-W)
-                      COMPUTE WS-RECT-H =
-                         FUNCTION INTEGER(WS-MAP-CELL-H)
-
-                      CALL "rlDrawRectangle" USING
-                         BY VALUE WS-P1-X WS-P1-Y WS-RECT-W
-                         WS-RECT-H, WS-MAP-WALL
-                   END-IF
-               END-PERFORM
-         END-PERFORM.
-
-         PERFORM VARYING WS-X FROM 1 BY 1
-                   UNTIL WS-X > (WS-WORLD-WIDTH + 1)
-
-                   COMPUTE WS-P1-X =
-                       (WS-X - 1) * WS-MAP-CELL-W + MAP-X
-
-                   COMPUTE WS-P2-Y = MAP-Y + MAP-HEIGHT
-
-                   CALL "rlDrawLine" USING
-                       BY VALUE WS-P1-X MAP-Y
-                       WS-P1-X WS-P2-Y
-                       2
-         END-PERFORM.
-
-         PERFORM VARYING WS-Y FROM 1 BY 1
-                   UNTIL WS-Y > (WS-WORLD-WIDTH + 1)
-
-                   COMPUTE WS-P1-Y =
-                       (WS-Y - 1) * WS-MAP-CELL-H + MAP-Y
-
-                   COMPUTE WS-P2-X = MAP-X + MAP-WIDTH
-
-                   CALL "rlDrawLine" USING
-                       BY VALUE MAP-X WS-P1-Y
-                       WS-P2-X WS-P1-Y
-                       2
-         END-PERFORM.
-
-       RENDER-MAP-PLAYER.
-
-           MOVE 5.0 TO WS-RADIUS.
-
-           COMPUTE WS-TMP-X =
-             (MAP-WIDTH / WS-WORLD-WIDTH) * PLAYER-X + MAP-X
-
-           COMPUTE WS-TMP-Y =
-             (MAP-HEIGHT / WS-WORLD-HEIGHT) * PLAYER-Y + MAP-Y
-
-           MOVE FUNCTION INTEGER (WS-TMP-X) TO WS-P1-X.
-           MOVE FUNCTION INTEGER (WS-TMP-Y) TO WS-P1-Y.
-
-           CALL "rlDrawCircle" USING
-             BY VALUE WS-P1-X WS-P1-Y WS-RADIUS 6.
 
        END PROGRAM RAYCAST.
---
+++ src/minimap.cbl
@@ -0,0 +1,123 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MINIMAP.
+       AUTHOR. Olivier Delbos.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       COPY DD-WORLD-SIZE.
+       COPY DD-WORLD-DATA.
+
+       01 WS-MAP-CELL-W   COMP-1 VALUE ZERO.
+       01 WS-MAP-CELL-H   COMP-1 VALUE ZERO.
+
+       01 WS-MAP-WALL     PIC 9(1) VALUE ZERO.
+
+       01 WS-TMP-X         COMP-1 VALUE ZERO.
+       01 WS-TMP-Y         COMP-1 VALUE ZERO.
+
+       01 WS-X            PIC 9(2) VALUE ZERO.
+       01 WS-Y            PIC 9(2) VALUE ZERO.
+
+       01 WS-P1-X        PIC 9(3) VALUE ZERO.
+       01 WS-P1-Y        PIC 9(3) VALUE ZERO.
+
+       01 WS-P2-X        PIC 9(3) VALUE ZERO.
+       01 WS-P2-Y        PIC 9(3) VALUE ZERO.
+
+       01 WS-RECT-W       PIC 9(3) VALUE ZERO.
+       01 WS-RECT-H       PIC 9(3) VALUE ZERO.
+
+       01 WS-RADIUS       COMP-1 VALUE ZERO.
+
+       LINKAGE SECTION.
+
+       COPY DD-GAME-STATE.
+
+       PROCEDURE DIVISION USING GAME-STATE.
+
+           PERFORM RENDER-MAP.
+           PERFORM RENDER-MAP-PLAYER.
+
+           EXIT PROGRAM.
+
+       RENDER-MAP.
+
+         CALL "rlDrawRectangle" USING
+               BY VALUE MAP-X MAP-Y MAP-WIDTH
+               MAP-HEIGHT, 1
+
+         COMPUTE WS-MAP-CELL-W = MAP-WIDTH / WS-WORLD-WIDTH
+         COMPUTE WS-MAP-CELL-H = MAP-HEIGHT / WS-WORLD-HEIGHT
+
+         PERFORM VARYING WS-Y FROM 1 BY 1
+                   UNTIL WS-Y > WS-WORLD-HEIGHT
+
+               PERFORM VARYING WS-X FROM 1 BY 1
+                     UNTIL WS-X > WS-WORLD-WIDTH
+
+                   MOVE WS-WORLD(WS-Y, WS-X) TO WS-MAP-WALL
+
+                   IF WS-MAP-WALL > 0 THEN
+                      COMPUTE WS-P1-X =
+                         (WS-X - 1) * WS-MAP-CELL-W + MAP-X
+
+                      COMPUTE WS-P1-Y =
+                         (WS-Y - 1) * WS-MAP-CELL-H + MAP-Y
+
+                      COMPUTE WS-RECT-W =
+                         FUNCTION INTEGER(WS-MAP-CELL-W)
+                      COMPUTE WS-RECT-H =
+                         FUNCTION INTEGER(WS-MAP-CELL-H)
+
+                      CALL "rlDrawRectangle" USING
+                         BY VALUE WS-P1-X WS-P1-Y WS-RECT-W
+                         WS-RECT-H, WS-MAP-WALL
+                   END-IF
+               END-PERFORM
+         END-PERFORM.
+
+         PERFORM VARYING WS-X FROM 1 BY 1
+                   UNTIL WS-X > (WS-WORLD-WIDTH + 1)
+
+                   COMPUTE WS-P1-X =
+                       (WS-X - 1) * WS-MAP-CELL-W + MAP-X
+
+                   COMPUTE WS-P2-Y = MAP-Y + MAP-HEIGHT
+
+                   CALL "rlDrawLine" USING
+                       BY VALUE WS-P1-X MAP-Y
+                       WS-P1-X WS-P2-Y
+                       2
+         END-PERFORM.
+
+         PERFORM VARYING WS-Y FROM 1 BY 1
+                   UNTIL WS-Y > (WS-WORLD-WIDTH + 1)
+
+                   COMPUTE WS-P1-Y =
+                       (WS-Y - 1) * WS-MAP-CELL-H + MAP-Y
+
+                   COMPUTE WS-P2-X = MAP-X + MAP-WIDTH
+
+                   CALL "rlDrawLine" USING
+                       BY VALUE MAP-X WS-P1-Y
+                       WS-P2-X WS-P1-Y
+                       2
+         END-PERFORM.
+
+       RENDER-MAP-PLAYER.
+
+           COMPUTE WS-TMP-X =
+             (MAP-WIDTH / WS-WORLD-WIDTH) * PLAYER-X + MAP-X.
+
+           COMPUTE WS-TMP-Y =
+             (MAP-HEIGHT / WS-WORLD-HEIGHT) * PLAYER-Y + MAP-Y.
+
+           MOVE FUNCTION INTEGER (WS-TMP-X) TO WS-P1-X.
+           MOVE FUNCTION INTEGER (WS-TMP-Y) TO WS-P1-Y.
+
+           MOVE 5.0 TO WS-RADIUS.
+           CALL "rlDrawCircle" USING
+             BY VALUE WS-P1-X WS-P1-Y WS-RADIUS 6.
+
+       END PROGRAM MINIMAP.
