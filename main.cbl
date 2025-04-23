d419a3145fe721fe8b3dd0ef68c210e317ad540b
odelbos <od@phibox.com>
2025-04-23 14:26:01 +0200

    initial commit

---
+++ main.cbl
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. RAYCAST.
+       AUTHOR. Olivier Delbos.
+
+       PROCEDURE DIVISION.
+
+           DISPLAY "hello world!".
+
+           STOP RUN.
+
+       END PROGRAM RAYCAST.
---
+++ make.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cobc -x -o raycast main.cbl
