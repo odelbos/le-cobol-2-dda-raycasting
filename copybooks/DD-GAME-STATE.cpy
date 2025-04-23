c47ecc5897db86dad768541f2f07f51e5a521095
odelbos <od@phibox.com>
2025-04-23 18:56:40 +0200

    do not compute map ratio on each frame

--- copybooks/DD-GAME-STATE.cpy
+++ copybooks/DD-GAME-STATE.cpy
@@ -10,6 +10,11 @@
              10 MAP-WIDTH      PIC 999 VALUE ZERO.
              10 MAP-HEIGHT     PIC 999 VALUE ZERO.
 
+      * Map ratio
+           05 MAP-RATIO.
+             10 MAP-RATIO-X      COMP-1 VALUE ZERO.
+             10 MAP-RATIO-Y      COMP-1 VALUE ZERO.
+
       * Player points in world coordinates
            05 PLAYER.
              10 PLAYER-X       COMP-1 VALUE ZERO.
--- src/minimap.cbl
+++ src/minimap.cbl
@@ -8,27 +8,24 @@
        COPY DD-WORLD-SIZE.
        COPY DD-WORLD-DATA.
 
-       01 WS-MAP-CELL-W   COMP-1 VALUE ZERO.
-       01 WS-MAP-CELL-H   COMP-1 VALUE ZERO.
+       01 WS-MAP-WALL      PIC 9(1) VALUE ZERO.
 
-       01 WS-MAP-WALL     PIC 9(1) VALUE ZERO.
+       01 WS-X             PIC 9(2) VALUE ZERO.
+       01 WS-Y             PIC 9(2) VALUE ZERO.
 
        01 WS-TMP-X         COMP-1 VALUE ZERO.
        01 WS-TMP-Y         COMP-1 VALUE ZERO.
 
-       01 WS-X            PIC 9(2) VALUE ZERO.
-       01 WS-Y            PIC 9(2) VALUE ZERO.
+       01 WS-P1-X          PIC 9(3) VALUE ZERO.
+       01 WS-P1-Y          PIC 9(3) VALUE ZERO.
 
-       01 WS-P1-X        PIC 9(3) VALUE ZERO.
-       01 WS-P1-Y        PIC 9(3) VALUE ZERO.
+       01 WS-P2-X          PIC 9(3) VALUE ZERO.
+       01 WS-P2-Y          PIC 9(3) VALUE ZERO.
 
-       01 WS-P2-X        PIC 9(3) VALUE ZERO.
-       01 WS-P2-Y        PIC 9(3) VALUE ZERO.
+       01 WS-RECT-W        PIC 9(3) VALUE ZERO.
+       01 WS-RECT-H        PIC 9(3) VALUE ZERO.
 
-       01 WS-RECT-W       PIC 9(3) VALUE ZERO.
-       01 WS-RECT-H       PIC 9(3) VALUE ZERO.
-
-       01 WS-RADIUS       COMP-1 VALUE ZERO.
+       01 WS-RADIUS        COMP-1 VALUE ZERO.
 
        LINKAGE SECTION.
 
@@ -47,9 +44,6 @@
                BY VALUE MAP-X MAP-Y MAP-WIDTH
                MAP-HEIGHT, 1
 
-         COMPUTE WS-MAP-CELL-W = MAP-WIDTH / WS-WORLD-WIDTH
-         COMPUTE WS-MAP-CELL-H = MAP-HEIGHT / WS-WORLD-HEIGHT
-
          PERFORM VARYING WS-Y FROM 1 BY 1
                    UNTIL WS-Y > WS-WORLD-HEIGHT
 
@@ -59,16 +53,11 @@
                    MOVE WS-WORLD(WS-Y, WS-X) TO WS-MAP-WALL
 
                    IF WS-MAP-WALL > 0 THEN
-                      COMPUTE WS-P1-X =
-                         (WS-X - 1) * WS-MAP-CELL-W + MAP-X
+                      COMPUTE WS-P1-X = (WS-X - 1) * MAP-RATIO-X + MAP-X
+                      COMPUTE WS-P1-Y = (WS-Y - 1) * MAP-RATIO-Y + MAP-Y
 
-                      COMPUTE WS-P1-Y =
-                         (WS-Y - 1) * WS-MAP-CELL-H + MAP-Y
-
-                      COMPUTE WS-RECT-W =
-                         FUNCTION INTEGER(WS-MAP-CELL-W)
-                      COMPUTE WS-RECT-H =
-                         FUNCTION INTEGER(WS-MAP-CELL-H)
+                      COMPUTE WS-RECT-W = FUNCTION INTEGER(MAP-RATIO-X)
+                      COMPUTE WS-RECT-H = FUNCTION INTEGER(MAP-RATIO-Y)
 
                       CALL "rlDrawRectangle" USING
                          BY VALUE WS-P1-X WS-P1-Y WS-RECT-W
@@ -80,38 +69,31 @@
          PERFORM VARYING WS-X FROM 1 BY 1
                    UNTIL WS-X > (WS-WORLD-WIDTH + 1)
 
-                   COMPUTE WS-P1-X =
-                       (WS-X - 1) * WS-MAP-CELL-W + MAP-X
+            COMPUTE WS-P1-X = (WS-X - 1) * MAP-RATIO-X + MAP-X
+            COMPUTE WS-P2-Y = MAP-Y + MAP-HEIGHT
 
-                   COMPUTE WS-P2-Y = MAP-Y + MAP-HEIGHT
-
-                   CALL "rlDrawLine" USING
-                       BY VALUE WS-P1-X MAP-Y
-                       WS-P1-X WS-P2-Y
-                       2
+            CALL "rlDrawLine" USING
+                BY VALUE WS-P1-X MAP-Y
+                WS-P1-X WS-P2-Y
+                2
          END-PERFORM.
 
          PERFORM VARYING WS-Y FROM 1 BY 1
                    UNTIL WS-Y > (WS-WORLD-WIDTH + 1)
 
-                   COMPUTE WS-P1-Y =
-                       (WS-Y - 1) * WS-MAP-CELL-H + MAP-Y
+            COMPUTE WS-P1-Y = (WS-Y - 1) * MAP-RATIO-Y + MAP-Y
+            COMPUTE WS-P2-X = MAP-X + MAP-WIDTH
 
-                   COMPUTE WS-P2-X = MAP-X + MAP-WIDTH
-
-                   CALL "rlDrawLine" USING
-                       BY VALUE MAP-X WS-P1-Y
-                       WS-P2-X WS-P1-Y
-                       2
+            CALL "rlDrawLine" USING
+                BY VALUE MAP-X WS-P1-Y
+                WS-P2-X WS-P1-Y
+                2
          END-PERFORM.
 
        RENDER-MAP-PLAYER.
 
-           COMPUTE WS-TMP-X =
-             (MAP-WIDTH / WS-WORLD-WIDTH) * PLAYER-X + MAP-X.
-
-           COMPUTE WS-TMP-Y =
-             (MAP-HEIGHT / WS-WORLD-HEIGHT) * PLAYER-Y + MAP-Y.
+           COMPUTE WS-TMP-X = MAP-RATIO-X * PLAYER-X + MAP-X.
+           COMPUTE WS-TMP-Y = MAP-RATIO-Y * PLAYER-Y + MAP-Y.
 
            MOVE FUNCTION INTEGER (WS-TMP-X) TO WS-P1-X.
            MOVE FUNCTION INTEGER (WS-TMP-Y) TO WS-P1-Y.
